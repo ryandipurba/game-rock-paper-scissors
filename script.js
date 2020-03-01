683eb70e8c144d3a0d07c83ab0e3922c4c964d66
Ryandi Purba <purbaryandi@gmail.com>
2020-03-01 10:06:23 +0700

    create score

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ function compChoice() {
 
 function getResult(comp, player) {
     if (player == comp) return "Draw";
-    if (player == 'gajah') return (comp == 'orang') ? "KALAH" : "MENANG!";
+    if (player == 'gajah') return (comp == 'semut') ? "KALAH" : "MENANG!";
     if (player == 'orang') return (comp == 'gajah') ? "KALAH" : "MENANG!";
     if (player == 'semut') return (comp == 'orang') ? "KALAH" : "MENANG!";
+
 
 }
 
@@ -32,18 +33,31 @@ function random() {
 
 const choice = document.querySelectorAll('.area-player ul li img')
 choice.forEach(function (pil) {
+    let playerScore = 0;
+    let compScore = 0;
     pil.addEventListener('click', function () {
+
+
         const comp = compChoice();
         const playerChoice = pil.className;
         const result = getResult(comp, playerChoice);
         random();
 
         setTimeout(function () {
+
             const imgComputer = document.querySelector('.img-komputer');
             imgComputer.setAttribute('src', 'img/' + comp + '.png ');
 
+            if (result == "MENANG!") playerScore += 1;
+            if (result == "KALAH") compScore += 1;
+            console.log(playerScore);
+
             const info = document.querySelector('.info');
+            const scoreP = document.querySelector('.playerScore');
+            const scoreC = document.querySelector('.compScore');
             info.innerHTML = result;
+            scoreC.innerHTML = compScore;
+            scoreP.innerHTML = playerScore;
         }, 1000)
     })
 })
