02dedcd55460c7513bd7f54513cde30312a2803b
Ryandi Purba <purbaryandi@gmail.com>
2020-03-01 08:22:58 +0700

    create function get result

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function compChoice() {
     if (comp >= 0.34 && comp < 0.67) return "orang";
     return "semut"
 }
+
+function getResult(comp, player) {
+    if (player == comp) return "Draw";
+    if (player == 'gajah') return (comp == 'orang') ? "KALAH" : "MENANG!";
+    if (player == 'orang') return (comp == 'gajah') ? "KALAH" : "MENANG!";
+    if (player == 'semut') return (comp == 'orang') ? "KALAH" : "MENANG!";
+
+}
