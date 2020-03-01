765c704d562a02c4635ce02d47f289e55b2e2b70
Ryandi Purba <purbaryandi@gmail.com>
2020-03-01 09:01:43 +0700

    create function random and choice player

--- script.js
+++ script.js
@@ -12,3 +12,38 @@ function getResult(comp, player) {
     if (player == 'semut') return (comp == 'orang') ? "KALAH" : "MENANG!";
 
 }
+
+function random() {
+
+    const imgComputer = document.querySelector('.img-komputer');
+    const img = ['gajah', 'semut', 'orang'];
+    let i = 0;
+    const start = new Date().getTime();
+    setInterval(function () {
+        if (new Date().getTime() - start > 1000) {
+            clearInterval;
+            return
+        }
+        imgComputer.setAttribute('src', 'img/' + img[i++] + '.png');
+        if (i == img.length) i = 0;
+    }, 100);
+}
+
+
+const choice = document.querySelectorAll('.area-player ul li img')
+choice.forEach(function (pil) {
+    pil.addEventListener('click', function () {
+        const comp = compChoice();
+        const playerChoice = pil.className;
+        const result = getResult(comp, playerChoice);
+        random();
+
+        setTimeout(function () {
+            const imgComputer = document.querySelector('.img-komputer');
+            imgComputer.setAttribute('src', 'img/' + comp + '.png ');
+
+            const info = document.querySelector('.info');
+            info.innerHTML = result;
+        }, 1000)
+    })
+})
